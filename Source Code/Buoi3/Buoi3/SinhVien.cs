727d4f813123e92e79c4e54504c3ebf7f2907932
Huynh Hoang Chuong <chuonghufisv1908@gmail.com>
2023-08-30 15:56:54 +0700

    Buoi3

---
+++ Source Code/Buoi3/Buoi3/SinhVien.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buoi3
+{
+    class SinhVien
+    {
+        int MaSV;
+
+        public int MASV
+        {
+            get { return MaSV; }
+            set { MaSV = value; }
+        }
+        string Hoten;
+
+        public string HoTen
+        {
+            get { return Hoten; }
+            set { Hoten = value; }
+        }
+        double diemLT, diemTH;
+
+        public double DiemTH
+        {
+            get { return diemTH; }
+            set { diemTH = value; }
+        }
+
+        public double DiemLT
+        {
+            get { return diemLT; }
+            set { diemLT = value; }
+        }
+
+        public SinhVien()
+        { 
+        }
+
+        public SinhVien(int MaSV, string HoTen, double diemLT, double diemTH)
+        {
+            this.MaSV = MaSV;
+            this.HoTen = HoTen;
+            this.diemLT = diemLT;
+            this.diemTH = diemTH;
+        }
+
+        public double diemTB()
+        {
+            return ((diemLT + diemTH)/2);
+        }
+    }
+}
